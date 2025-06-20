331a2d6c7e37a782c3681122380ac50b9580c9a4
Sugashini S <sugashinisudalaimuthu@gmail.com>
2025-06-20 09:09:09 +0530

    Updated Todo App with inline edit and dark mode

--- app.js
+++ app.js
@@ -1,74 +1,95 @@
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
+const themeToggleBtn = document.getElementById("theme-toggle");
 
 let allTodos = getTodos();
 updateTodoList();
-
-todoForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    addTodo();
-})
-function addTodo(){
-    const todoText = todoInput.value.trim();
-    if(todoText.length > 0){
-        const todoObject = {
-            text: todoText,
-            completed: false
-        }
-        allTodos.push(todoObject);
-        updateTodoList();
-        saveTodos();
-        todoInput.value = "";
-    }  
-}function updateTodoList(){
-    todoListUL.innerHTML = "";
-    allTodos.forEach((todo, todoIndex)=>{
-        todoItem = createTodoItem(todo, todoIndex);
-        todoListUL.append(todoItem);
-    })
-}
-function createTodoItem(todo, todoIndex){
-    const todoId = "todo-"+todoIndex;
-    const todoLI = document.createElement("li");
-    const todoText = todo.text;
-    todoLI.className = "todo";
-    todoLI.innerHTML = ` <input type="checkbox" id="${todoId}">
-        <label class="custom-checkbox" for="${todoId}">
-            <svg fill="transparent"xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
-        </label>
-        <label for="${todoId}" class="todo-text">
-            ${todoText}
-        </label>
-        <button class="delete-button">
-            <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-        </button>`
-    const deleteButton = todoLI.querySelector(".delete-button");
-    deleteButton.addEventListener("click", ()=>{
-        deleteTodoItem(todoIndex);
-    })
-    const checkbox = todoLI.querySelector("input");
-    checkbox.addEventListener("change", ()=>{
-        allTodos[todoIndex].completed = checkbox.checked;
-        saveTodos();
-    })
-    checkbox.checked = todo.completed;
-    return todoLI;
-}
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
-    saveTodos();
+todoForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  addTodo();
+});
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0) {
+    const todoObject = {
+      text: todoText,
+      completed: false
+    };
+    allTodos.push(todoObject);
     updateTodoList();
+    saveTodos();
+    todoInput.value = "";
+  }
 }
-function saveTodos(){
-    const todosJson = JSON.stringify(allTodos);
-    localStorage.setItem("todos", todosJson);
+function updateTodoList() {
+  todoListUL.innerHTML = "";
+  allTodos.forEach((todo, todoIndex) => {
+    const todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
 }
-function getTodos(){
-    const todos = localStorage.getItem("todos") || "[]";
-    return JSON.parse(todos);
+
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  todoLI.className = "todo";
+
+  todoLI.innerHTML = `
+    <input type="checkbox" id="${todoId}">
+    <label class="custom-checkbox" for="${todoId}">
+      <img src="icons/done.svg" alt="Done" />
+    </label>
+    <label for="${todoId}" class="todo-text">${todo.text}</label>
+    <div class="actions">
+      <button class="edit-button">
+        <img src="icons/pencil.svg" alt="Edit" />
+      </button>
+      <button class="delete-button">
+        <img src="icons/delete.svg" alt="Delete" />
+      </button>
+    </div>
+  `;
+
+  const checkbox = todoLI.querySelector("input[type='checkbox']");
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+
+  const deleteButton = todoLI.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
+
+  const editButton = todoLI.querySelector(".edit-button");
+  editButton.addEventListener("click", () => {
+    const newText = prompt("Edit your task:", allTodos[todoIndex].text);
+    if (newText !== null && newText.trim() !== "") {
+      allTodos[todoIndex].text = newText.trim();
+      saveTodos();
+      updateTodoList();
+    }
+  });
+
+  return todoLI;
 }
-const themeToggleBtn = document.getElementById("theme-toggle");
+
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+
+function saveTodos() {
+  const todosJson = JSON.stringify(allTodos);
+  localStorage.setItem("todos", todosJson);
+}
+function getTodos() {
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
+}
 themeToggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   saveTheme();
@@ -86,3 +107,71 @@ function loadTheme() {
   }
 }
 loadTheme();
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  todoLI.className = "todo";
+
+  todoLI.innerHTML = `
+    <input type="checkbox" id="${todoId}">
+    <label class="custom-checkbox" for="${todoId}">
+      <img src="icons/done.svg" alt="Done" />
+    </label>
+    <label for="${todoId}" class="todo-text">${todo.text}</label>
+    <div class="actions">
+      <button class="edit-button">
+        <img src="icons/pencil.svg" alt="Edit" />
+      </button>
+      <button class="delete-button">
+        <img src="icons/delete.svg" alt="Delete" />
+      </button>
+    </div>
+  `;
+
+  const checkbox = todoLI.querySelector("input[type='checkbox']");
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+
+  const deleteButton = todoLI.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
+  });
+
+  const editButton = todoLI.querySelector(".edit-button");
+  editButton.addEventListener("click", () => {
+    const textLabel = todoLI.querySelector(".todo-text");
+
+    const inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.value = todo.text;
+    inputField.className = "edit-input";
+    inputField.setAttribute("aria-label", "Edit task");
+
+    todoLI.replaceChild(inputField, textLabel);
+    inputField.focus();
+
+    inputField.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        saveEdit();
+      }
+    });
+
+    inputField.addEventListener("blur", () => {
+      saveEdit();
+    });
+
+    function saveEdit() {
+      const updatedText = inputField.value.trim();
+      if (updatedText) {
+        allTodos[todoIndex].text = updatedText;
+      }
+      saveTodos();
+      updateTodoList();
+    }
+  });
+
+  return todoLI;
+}
